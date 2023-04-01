62c5dec7f3200f22f8aa79aa267d4422999bbcdb
Muazkhalid786 <itzmajo786@gmail.com>
2023-04-01 14:17:45 +0500

    Complete PRoject

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log("Hello")
+
+var typed = new Typed('#course', {
+    strings: ['C++ <img class="langImg"  src="../img/cpp.png" >', '', 'Python <img class="langImg"  src="../img/python.png" >','C# <img class="langImg"  src="../img/csharp.png" >', 'JavaScript   <img class="langImg"  src="../img/javascript.png" >  ', 'TypeScript    <img class="langImg"  src="../img/typescript.png" >  ',  'Microsoft Powerpoint  <img class="langImg"  src="../img/powerpoint.png" > '],
+    smartBackspace: true, // Default value
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+  });
